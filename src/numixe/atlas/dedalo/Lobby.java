d4a7fdc82c19428a8018bc57debe2c2dae54ef7e
Numixe <numixe@gmail.com>
2016-09-20 19:14:44 +0200

    Add

--- src/numixe/atlas/dedalo/Lobby.java
+++ src/numixe/atlas/dedalo/Lobby.java
@@ -5,9 +5,12 @@ import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class Lobby implements Listener {
@@ -44,8 +47,26 @@ public class Lobby implements Listener {
 		Block block = event.getClickedBlock();
 		
 		if (block.getType() == Material.SIGN) {
-			
-			// verifica il testo
+		    if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+		      if ((block.getState() instanceof Sign))
+		      {
+		        Sign sign = (Sign) block.getState();
+		        if (sign.getLine(0).equalsIgnoreCase("[KoaSign]")) {
+		          p.sendMessage("Bravo! Hai Cliccato il Cartello!");
+		        }
+		      }
+		    }
 		}
 	}
+	
+	@EventHandler
+	  public void onSignCreate(SignChangeEvent e)
+	  {
+	    Player p = e.getPlayer();
+	    if (e.getLine(0).equalsIgnoreCase("[KoaSign]"))
+	    {
+	      e.setLine(0, "[KoaSign]");
+	      p.sendMessage("Il Cartello e' stato creato con successo");
+	    }
+	  }
 }
